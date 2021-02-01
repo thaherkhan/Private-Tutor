26f0b8dd0ca06e457611ff4fd4369341c3f3d69c
thaherkhan <38291466+thaherkhan@users.noreply.github.com>
2021-02-02 00:17:22 +0600

    Add files via upload

---
+++ Include/Home.php
@@ -0,0 +1,57 @@
+
+<html>
+
+	<head>
+	
+		<link href="design.css" rel="stylesheet">
+		<title> About </title>
+	
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+				
+	</head>
+	
+	<div class="head">
+		
+		<div class="header">
+			<h1 style="color: red">Welcome to <br/> Private Tutor Finder System</h1>
+		</div>
+		
+		<div class="loginfo">
+			<th>
+				<h3 ><a href="Login.php">Login</a>
+				or
+				<a href="register.php">Register</a></h3>
+			</th>
+		</div>
+	</div>
+		
+	<div class="menu">
+		<ul>
+			<li><a href="index.php">HOME</a></li>
+			<li><a href="search.php">SEARCH</a></li>
+			
+			<li class="dropdown">
+				<a href="javascript:void(0)" class="dropbtn">FOR STUDENT</a>
+				<div class="dropdown-content">
+					<a href="search.php"> Find Tutor </a>
+					<a href="about.php"> How Our Service Works </a>
+					<a href="Singup3std.php"> Register </a>
+				</div>
+			</li>
+			
+			<li class="dropdown">
+				<a href="javascript:void(0)" class="dropbtn">FOR  TUTOR</a>
+				<div class="dropdown-content">
+					<a href="search.php"> Find Student </a>
+					<a href="about.php"> How Our Service Works </a>
+					<a href="Singup.php"> Register </a>
+				</div>
+			</li>
+			
+			<li><a href="contact_us.php">CONTACT US</a></li>
+			<li><a href="about.php">ABOUT</a></li>
+		</ul>
+	</div>
+
+</html>
